25e1e45677f0e07fda3527ac040b6470643fea68
PStewart1 <4920@holbertonstudents.com>
2023-03-14 06:48:49 -0500

    Simple callback

---
+++ 0x00-Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,19 @@
+function createElement (data) {
+  const paragraph = document.createElement('p');
+  paragraph.innerHTML = data;
+  document.body.appendChild(paragraph);
+}
+
+function queryWikipedia (callback) {
+  const article = new XMLHttpRequest();
+  article.onreadystatechange = () => {
+    if (article.readyState === XMLHttpRequest.DONE) {
+      callback(article.responseText);
+    }
+  };
+
+  article.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*');
+  article.send();
+}
+
+queryWikipedia(createElement);
